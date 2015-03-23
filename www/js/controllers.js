70bf1e6f19ae94a68efaff36e3996dc65776e429
ganaraj <ganarajpr@gmail.com>
2015-03-23 00:03:09 +0000

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,37 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+        $scope.fbLogin = function() {
+            openFB.login(
+                function(response) {
+                    if (response.status === 'connected') {
+                        console.log('Facebook login succeeded');
+                        $scope.closeLogin();
+                    } else {
+                        alert('Facebook login failed');
+                    }
+                },
+                {scope: 'email,publish_actions'});
+        };
+
+        $scope.closeLogin = function(){
+
+        }
+    })
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
